94b8975eba31f6dd7a9ef089c58670c772d0d6f6
Rafi Khan <rafi.fa.khan@gmail.com>
2016-06-08 01:46:31 +0200

    api example

--- Day8/script.js
+++ Day8/script.js
@@ -1,7 +1,38 @@
 $(document).ready(function() {
-  $("#todo-input").keyup(function(e) {
-    if (e.keyCode == 13) {
-      addToList();
-    }
+  var url = "https://api.nytimes.com/svc/mostpopular/v2/mostemailed//.json";
+  $.ajax({
+    url: url,
+    method: 'GET',
+  }).done(function(result) {
+    displayResults(results);
+  }).fail(function(err) {
+    throw err;
   });
+
+  var results = [
+    {title: "Intro coding class breaks new learning records", byline: "Rafi Khan", url: "http://google.com"},
+    {title: "In bold move, iXperience expands to Antarctica", byline: "Aaron Fuchs", url: "http://google.com"},
+    {title: "When in doubt, just Google it and leave me alone", byline: "Allie Ivener", url: "http://google.com"},
+  ]
+  displayResults(results);
 });
+
+function displayResults(results) {
+  for (var i = 0; i<results.length; i++) {
+    var res = results[i];
+    var div = $("<div></div>"); // Containing div
+    div.addClass("article");
+    var p = $("<p></p>"); // Title with link
+    var a = $("<a></a>");
+    a.html(res["title"]);
+    a.attr("href", res["url"]);
+    a.attr("target", "_blank");
+    var span = $("<span></span>"); // Byline
+    span.html(res["byline"]);
+    span.addClass("byline");
+    // Tie it all together
+    p.append(a);
+    div.append(p).append(span);
+    $("#list").append(div);
+  }
+}
