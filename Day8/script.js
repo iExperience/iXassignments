d27179fa5a9f18a69b9c226a92513889999db744
Rafi Khan <rafi.fa.khan@gmail.com>
2016-06-08 00:01:56 +0200

    Step 3

--- Day8/script.js
+++ Day8/script.js
@@ -1,7 +1,14 @@
 $(document).ready(function() {
   $("#todo-input").keyup(function(e) {
     if (e.keyCode == 13) {
-      addToList();
+      console.log("enter!")
+      addToList($("#todo-input").val());
     }
   });
 });
+
+function addToList(value) {
+  var div = $("<div></div>").html(value);
+  div.addClass("list-item");
+  $("#list").append(div);
+}
