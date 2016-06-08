f9c1dadedbb8b9aa512ef38f40fa396b3e03e1b9
Allie Ivener <adi2hf@virginia.edu>
2016-06-08 11:45:48 +0200

    nyt search added

--- Day8/script.js
+++ Day8/script.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
   var url = "https://api.nytimes.com/svc/topstories/v2/opinion.json";
-  var key = "6c1830c231564612bbf5484ce7933e27";
+  var key = "f22d436400ff477a9ea8571b8bf7a9f1";
   $.ajax({
     url: url + "?api-key=" + key,
     method: 'GET',
@@ -10,7 +10,51 @@ $(document).ready(function() {
   }).fail(function(err) {
     throw err;
   });
+
+  $(".search").keypress(function(e) {
+    var key = e.keyCode;
+    if(key === 13) {
+      console.log("value", e.currentTarget.value);
+      var searchTerm = e.currentTarget.value;
+      var searchURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
+      searchURL += '?' + $.param({
+        'api-key': "f22d436400ff477a9ea8571b8bf7a9f1",
+        'q': searchTerm
+      });
+      $.ajax({
+        url: searchURL,
+        method: 'GET',
+      }).done(function(response) {
+        console.log("done!", response);
+        searchResults(response.response.docs);
+      }).fail(function(err) {
+        throw err;
+      });
+    }
+  });
 });
+
+function searchResults(results) {
+  $("#list").html("");
+  console.log("search", results);
+  for (var i = 0; i<results.length; i++) {
+    var res = results[i];
+    var div = $("<div></div>"); // Containing div
+    div.addClass("article");
+    var p = $("<p></p>"); // Title with link
+    var a = $("<a></a>");
+    a.html(res["headline"]["main"]);
+    a.attr("href", res["web_url"]);
+    a.attr("target", "_blank");
+    var span = $("<span></span>"); // Byline
+    span.html(res["byline"]["original"]);
+    span.addClass("byline");
+    // Tie it all together
+    p.append(a);
+    div.append(p).append(span);
+    $("#list").append(div);
+  }
+}
 
 function displayResults(results) {
   for (var i = 0; i<results.length; i++) {
@@ -31,3 +75,4 @@ function displayResults(results) {
     $("#list").append(div);
   }
 }
+
