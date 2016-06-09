87e29a5c9e00c38f5ed08c8a7c1db81c1d2e08a5
Rafi Khan <rafi.fa.khan@gmail.com>
2016-06-09 17:01:43 +0200

    Day 9 afternoon code

---
+++ Day9/_script.js
@@ -0,0 +1,34 @@
+$(document).ready(function() {
+  var url = "https://api.nytimes.com/svc/topstories/v2/opinion.json";
+  var key = "6c1830c231564612bbf5484ce7933e27";
+  $.ajax({
+    url: url + "?api-key=" + key,
+    method: 'GET',
+    success: function(response) {
+      displayResults(response.results);
+    },
+    error: function(error) {
+      console.log("Error!");
+    }
+  });
+});
+
+function displayResults(results) {
+  for (var i = 0; i<results.length; i++) {
+    var res = results[i];
+    var div = $("<div></div>"); // Containing div
+    div.addClass("article");
+    var p = $("<p></p>"); // Title with link
+    var a = $("<a></a>");
+    a.html(res["title"]);
+    a.attr("href", res["url"]);
+    a.attr("target", "_blank");
+    var span = $("<span></span>"); // Byline
+    span.html(res["byline"]);
+    span.addClass("byline");
+    // Tie it all together
+    p.append(a);
+    div.append(p).append(span);
+    $("#list").append(div);
+  }
+}
--- Day9/script.js
+++ Day9/script.js
@@ -1,33 +1,37 @@
+// User does an action
+  // Load the page (change to when user presses enter in the search bar)
+// Javascript makes a request
+  // to: The New York API
+// My page uses the data
+  // Display top news stories on the page
+
 $(document).ready(function() {
-  var url = "https://api.nytimes.com/svc/topstories/v2/opinion.json";
-  var key = "6c1830c231564612bbf5484ce7933e27";
+  var url = "https://api.nytimes.com/svc/topstories/v2/home.json" +
+    "?api-key=6c1830c231564612bbf5484ce7933e27"
+
   $.ajax({
-    url: url + "?api-key=" + key,
-    method: 'GET',
-  }).done(function(response) {
-    console.log("done!", response);
-    displayResults(response.results);
-  }).fail(function(err) {
-    throw err;
+    url: url, // The URL you are requesting
+    method: "GET",
+    success: handleResponse,// A function that is run when the request is complete
+    error: function(error) {
+      alert("error!");
+    }
   });
 });
 
-function displayResults(results) {
-  for (var i = 0; i<results.length; i++) {
-    var res = results[i];
-    var div = $("<div></div>"); // Containing div
-    div.addClass("article");
-    var p = $("<p></p>"); // Title with link
-    var a = $("<a></a>");
-    a.html(res["title"]);
-    a.attr("href", res["url"]);
-    a.attr("target", "_blank");
-    var span = $("<span></span>"); // Byline
-    span.html(res["byline"]);
-    span.addClass("byline");
-    // Tie it all together
-    p.append(a);
-    div.append(p).append(span);
-    $("#list").append(div);
+function handleResponse(response) {
+  console.log(response);
+  // $("#list").html(response.results[].title);
+
+  // Show ALL THE TITLES
+  for (var i=0; i<response.results.length; i++) {
+    // console.log(response.results[i].title);
+    $("#list").append(response.results[i].title);
   }
 }
+
+
+
+
+
+
