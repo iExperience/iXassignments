02e45edc6e3fb794545c48939e3712d1afb4543c
Rafi Khan <rafi.fa.khan@gmail.com>
2016-06-15 17:36:45 +0200

    comments

--- Day14/script.js
+++ Day14/script.js
@@ -22,11 +22,15 @@ app.controller("FeedCtrl", function($scope, $http, $filter) {
   });
 
   $scope.addProp = function() {
+    // Mock code just to illustrate effect. We would instead make a call
+    // to the API here and get to get the prop that was created with these
+    // variables filled in. 
     $scope.newProp.positivity_score = 0.2;
     $scope.newProp.receiver =
       $filter('filter')($scope.brus, {id: $scope.newProp.receiver_id})[0];
     $scope.newProp.sender = ME;
     console.log($scope.newProp);
+    // End sample code.
     $scope.props.unshift($scope.newProp);
     $scope.newProp = {}
   }
