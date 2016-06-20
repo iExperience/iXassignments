c7bf4c7efe87e8369236b769657d53eea90af99e
Rafi Khan <rafi.fa.khan@gmail.com>
2016-06-20 12:18:37 +0200

    auth work

--- Day16/script.js
+++ Day16/script.js
@@ -1,39 +1,14 @@
 var app = angular.module("ChommiesApp", ["ngRoute", "firebase"]);
 var CHOMMIES_TOKEN = "ba1c55f06bd6b5f28f46f09ecd744287";
-var FIREBASE_URL = "https://project-220434664661779401.firebaseio.com";
-
-app.factory("Auth", function($firebaseAuth) {
-  return $firebaseAuth();
-});
-
-app.run(function($rootScope, $location) {
-  $rootScope.$on("$routeChangeError", function(event, next, previous, error) {
-    // We can catch the error thrown when the $requireSignIn promise is rejected
-    // and redirect the user back to the home page
-    if (error === "AUTH_REQUIRED") {
-      $location.path("/login");
-    }
-  });
-});
 
 app.config(function($routeProvider) {
   $routeProvider.when("/", {
     controller: "FeedCtrl",
-    templateUrl: "templates/feed.html",
-    resolve: {
-      firebaseUser: function(Auth) {
-        return Auth.$requireSignIn();
-      }
-    }
+    templateUrl: "templates/feed.html"
   })
   $routeProvider.when("/login", {
     controller: "LoginCtrl",
-    templateUrl: "templates/login.html",
-    resolve: {
-      firebaseUser: function(Auth) {
-        return Auth.$waitForSignIn();
-      }
-    }
+    templateUrl: "templates/login.html"
   })
   $routeProvider.when("/me", {
     controller: "MeCtrl",
@@ -41,49 +16,41 @@ app.config(function($routeProvider) {
   })
 });
 
-app.controller("LoginCtrl", function($scope, $location, Auth, firebaseUser) {
-  // var ref = firebase.database().ref();
-  console.log(firebaseUser);
-  $scope.firebaseUser = firebaseUser;
-
-  $scope.createUser = function() {
-    $scope.message = null;
-    $scope.error = null;
-    Auth.$createUserWithEmailAndPassword($scope.email, $scope.password)
-      .then(function(firebaseUser) {
-        $scope.firebaseUser = firebaseUser;
-        console.log(firebaseUser);
-        $scope.message = "User created with uid: " + firebaseUser.uid;
-      }).catch(function(error) {
-        $scope.error = error;
-      });
-  }
-
-  $scope.signIn = function() {
-    $scope.message = null;
-    $scope.error = null;
-    Auth.$signInWithEmailAndPassword($scope.email, $scope.password)
-      .then(function(firebaseUser) {
-        $scope.message = "Signed in!";
-        $scope.firebaseUser = firebaseUser;
-      }).catch(function(error) {
-        $scope.error = error;
-      });
-  }
-
-  Auth.$onAuthStateChanged(function(firebaseUser) {
+app.controller("LoginCtrl", function($scope, $location, $firebaseAuth) {
+  var auth = $firebaseAuth();
+  auth.$onAuthStateChanged(function(firebaseUser) {
     if (firebaseUser) {
       $location.path("/");
     } else {
-      $scope.message = "Logged out!"
+      $scope.error = "Please sign in";
     }
   });
+  $scope.signIn = function() {
+    $scope.message = null;
+    $scope.error = null;
+    auth.$signInWithPopup("facebook")
+      .then(function(result) {
+        console.log(result)
+        $scope.message = "Signed in!";
+        $scope.firebaseUser = result.user;
+      }).catch(function(error) {
+        $scope.error = error;
+      });
+  }
 });
 
-app.controller("FeedCtrl", function($scope, $http, $location, firebaseUser, Auth) {
-  $scope.firebaseUser = firebaseUser;
+app.controller("FeedCtrl", function($scope, $http, $location, $firebaseAuth) {
+  var auth = $firebaseAuth();
+  auth.$onAuthStateChanged(function(firebaseUser) {
+    if (firebaseUser) {
+      $scope.firebaseUser = firebaseUser;
+    } else {
+      $location.path("/login");
+    }
+  });
+
   $scope.logout = function() {
-    Auth.$signOut();
-    console.log("out");
+    auth.$signOut();
+    $location.path("/login");
   }
 });
